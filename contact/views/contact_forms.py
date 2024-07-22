f88cc8c9fe3bdf358d1d039ba24271e02d32984b
prazeresmath <matheus.prazeres@hotmail.com>
2024-07-22 18:36:25 -0300

    criação de validação

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,19 +1,23 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
+from django import forms
 
+from contact.forms import ContactForm
 from contact.models import Contact
 
-
 def create(request):
-
     context = {
-        
+        'form': ContactForm()
     }
+    
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
 
     return render(
         request,
         'contact/create.html',
         context
     )
-
